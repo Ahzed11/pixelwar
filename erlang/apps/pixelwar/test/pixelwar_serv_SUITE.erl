e2193509cab3cf7fb42f05593a30e1627293e15d
Alexandre Zenon <a.zenon@student.uclouvain.be>
2023-12-11 11:22:53 +0100

    Add test suite for piwelwar_serv

---
+++ erlang/apps/pixelwar/test/pixelwar_serv_SUITE.erl
@@ -0,0 +1,28 @@
+-module(pixelwar_serv_SUITE).
+-include_lib("stdlib/include/assert.hrl").
+-include_lib("common_test/include/ct.hrl").
+-compile(export_all).
+
+all() ->
+    [get_state_test_case].
+
+init_per_testcase(_Case, Config) ->
+    application:load(pixelwar),
+    {ok, Apps} = application:ensure_all_started([pixelwar]),
+    [{apps, Apps} | Config].
+
+end_per_testcase(_Case, Config) ->
+    [application:stop(App) || App <- lists:reverse(?config(apps, Config))],
+    Config.
+
+get_state_test_case() ->
+    [{doc, "Tries to get the current matrix as binary"},
+     {timetrap, timer:seconds(5)}].
+
+get_state_test_case(_Config) ->
+    pixelwar_matrix_serv:set_element(matrix, {42, 42, 12}),
+    pixelwar_matrix_serv:set_element(matrix, {42, 42, 42}),
+    pixelwar_matrix_serv:set_element(matrix, {11, 12, 13}),
+
+    MatrixAsBin = pixelwar_matrix_serv:get_state(matrix),
+    ?assertEqual(MatrixAsBin, <<11:16/little, 12:16/little, 13:16/little, 42:16/little, 42:16/little, 42:16/little>>).

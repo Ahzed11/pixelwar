1ce70851f2d4d79011b28236ea31bcbf3b247fef
Alexandre Zenon <a.zenon@student.uclouvain.be>
2023-12-18 12:26:27 +0100

    Handle no variable from env

--- erlang/apps/pixelwar/src/pixelwar_sup.erl
+++ erlang/apps/pixelwar/src/pixelwar_sup.erl
@@ -1,7 +1,7 @@
 -module(pixelwar_sup).
 
 -behaviour(supervisor).
-
+-define(DEFAULT_SIZE, 128).
 %% API
 -export([start_link/0]).
 -export([init/1]).
@@ -15,8 +15,14 @@ init(_Args) ->
         intensity => 10,
         period => 60},
 
-        {ok, Width} = application:get_env(pixelwar, matrix_width),
-        {ok, Height} = application:get_env(pixelwar, matrix_height),
+        Width = case application:get_env(pixelwar, matrix_width) of
+            {ok, Vw} -> Vw;
+            undefined -> ?DEFAULT_SIZE
+        end,
+        Height = case application:get_env(pixelwar, matrix_height) of
+            {ok, Vh} -> Vh;
+            undefined -> ?DEFAULT_SIZE
+        end,
 
         ChildSpecifications = [
         #{

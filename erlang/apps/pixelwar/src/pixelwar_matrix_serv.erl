e2682d9b0be45ea623622a2e33b918fd09282a58
Alexandre Zenon <ahzed11@protonmail.com>
2023-12-22 12:07:46 +0100

    Use appup plugin state transformation

--- erlang/apps/pixelwar/src/pixelwar_matrix_serv.erl
+++ erlang/apps/pixelwar/src/pixelwar_matrix_serv.erl
@@ -1,12 +1,17 @@
 -module(pixelwar_matrix_serv).
-
 -behaviour(gen_server).
+
+-record(state, {
+    pixels = #{} :: #{{non_neg_integer(), non_neg_integer()} => non_neg_integer()},
+    width = 128 :: non_neg_integer(),
+    height = 128 :: non_neg_integer()
+}).
+
+-state_record([state]).
 
 %% API
 -export([start_link/1, set_element/2, get_state/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
-
--record(pixelwar_matrix, {pixels, width, height}).
 
 start_link(Args) ->
     gen_server:start_link({local, matrix}, ?MODULE, Args, []).
@@ -21,7 +26,7 @@ get_state(Instance) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 init({Width, Height}) ->
-    {ok, #pixelwar_matrix{pixels=#{}, width=Width, height=Height}}.
+    {ok, #state{pixels=#{}, width=Width, height=Height}}.
 
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State};
@@ -31,7 +36,7 @@ handle_call(get_state, _From, State) ->
         {X, Y} = K,
         <<Acc/binary, X:16/little, Y:16/little, V:16/little>>
     end,
-    AsBinary = maps:fold(ToBinary, <<>>, State#pixelwar_matrix.pixels),
+    AsBinary = maps:fold(ToBinary, <<>>, State#state.pixels),
     {reply, AsBinary, State};
 
 handle_call(_Request, _From, State) ->
@@ -40,11 +45,11 @@ handle_call(_Request, _From, State) ->
 handle_cast({set_element, {X, Y, Color}}, State) ->
     Key = {X, Y},
     if
-        X >= State#pixelwar_matrix.width orelse X < 0 -> {noreply, State};
-        Y >= State#pixelwar_matrix.height orelse Y < 0 -> {noreply, State};
+        X >= State#state.width orelse X < 0 -> {noreply, State};
+        Y >= State#state.height orelse Y < 0 -> {noreply, State};
         true ->
-            NewPixels = maps:put(Key, Color, State#pixelwar_matrix.pixels),
-            NewState = State#pixelwar_matrix{pixels=NewPixels, width=State#pixelwar_matrix.width, height=State#pixelwar_matrix.height},
+            NewPixels = maps:put(Key, Color, State#state.pixels),
+            NewState = State#state{pixels=NewPixels, width=State#state.width, height=State#state.height},
             {noreply, NewState}
     end;
 
@@ -56,19 +61,6 @@ handle_info(_Info, State) ->
 
 terminate(_Reason, _State) ->
     ok.
-
-code_change("0.1.0", State, _Extra) ->
-    logger:info("Code change ! ~w", [State]),
-    Width = 128,
-    Height = 128,
-    IsInBound = fun({X, Y}, _V) -> X < Width andalso X >= 0 andalso Y < Height andalso Y >= 0 end,
-    FilteredState = maps:filter(IsInBound, State),
-    NewState = #pixelwar_matrix{pixels=FilteredState, width=Width, height=Height},
-    logger:info("New state ! ~w", [NewState]),
-    {ok, NewState};
-
-code_change({down, "0.1.0"}, State, _Extra) ->
-    {ok, State#pixelwar_matrix.pixels};
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.

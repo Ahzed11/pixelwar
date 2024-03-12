116ece3048f6ac15e2823a31aae1df049184a3b3
Alexandre Zenon <ahzed11@protonmail.com>
2024-03-12 12:15:55 +0100

    docker logic in its own module

--- test/distributed_SUITE.erl
+++ test/distributed_SUITE.erl
@@ -25,11 +25,14 @@ init_per_suite(Config) ->
     ct:print("Initializing suite..."),
     ct:log(info, ?LOW_IMPORTANCE, "Initializing suite...", []),
     Docker = os:find_executable("docker"),
-    build_image(),
+
+    OldVSN = ct:get_config(old_version),
+    NewVSN = ct:get_config(new_version),
     ReleaseName = ct:get_config(release_name),
+    ReleaseDir = ct:get_config(release_dir),
+    docker_helper:build_image({OldVSN, NewVSN}, ReleaseName, ReleaseDir),
 
     %% start two Docker containers
-
     {ok, Peer, Node} = peer:start(#{name => ReleaseName,
         connection => standard_io,
         exec => {Docker, ["run", "-h", "one", "-i", ReleaseName]}}),
@@ -51,7 +54,6 @@ init_per_suite(Config) ->
     true = peer:call(Peer, net_kernel, connect_node, [Node2]),
     %% verify that second peer node has only the first node visible
     [Node] = peer:call(Peer2, erlang, nodes, []),
-
 
     [{peers, [Peer, Peer2]}, {nodes, [Node, Node2]} | Config].
 
@@ -98,42 +100,3 @@ downgrade_case(_Config) ->
 
 after_downgrade_case(_Config) ->
     ok.
-
-% ========== HELPERS ==========
-
-build_image() ->
-    NewVSN = ct:get_config(new_version),
-    OldVSN = ct:get_config(old_version),
-    ReleaseName = ct:get_config(release_name),
-    NewReleaseName = ReleaseName ++ "-" ++ NewVSN,
-    OldReleaseName = ReleaseName ++ "-" ++ OldVSN,
-    ReleaseDir = ct:get_config(release_dir),
-
-    NewReleasePath = filename:join(ReleaseDir, NewReleaseName ++ ".tar.gz"),
-    file:copy(NewReleasePath, "./" ++ NewReleaseName ++ ".tar.gz"),
-
-    OldReleasePath = filename:join(ReleaseDir, OldReleaseName ++ ".tar.gz"),
-    file:copy(OldReleasePath, "./" ++ OldReleaseName ++ ".tar.gz"),
-
-    %% Create Dockerfile example, working only for Ubuntu 20.04
-    %% Expose port 4445, and make Erlang distribution to listen
-    %% on this port, and connect to it without EPMD
-    %% Set cookie on both nodes to be the same.
-    BuildScript = filename:join("./", "Dockerfile"),
-    Dockerfile =
-        "FROM ubuntu:22.04 as runner\n"
-        "EXPOSE 4445\n"
-        "WORKDIR /opt/" ++ ReleaseName ++  "\n"
-        "COPY [\"" ++ OldReleaseName ++ ".tar.gz\", \"" ++ NewReleaseName ++ ".tar.gz\"" ++ ", \"/tmp/\"]\n"
-        "RUN tar -zxvf /tmp/" ++ OldReleaseName ++ ".tar.gz -C /opt/" ++ ReleaseName ++ "\n"
-        "RUN mkdir /opt/" ++ ReleaseName ++ "/releases/" ++ NewVSN ++ "\n"
-        "RUN cp /tmp/" ++ NewReleaseName ++ ".tar.gz /opt/" ++ ReleaseName ++ "/releases/" ++ NewVSN ++ "/" ++ ReleaseName ++ ".tar.gz\n"
-        "ENTRYPOINT [\"/opt/" ++ ReleaseName ++ "/erts-" ++ erlang:system_info(version) ++
-        "/bin/dyn_erl\", \"-boot\", \"/opt/" ++ ReleaseName ++ "/releases/" ++ OldVSN ++ "/start\","
-        " \"-kernel\", \"inet_dist_listen_min\", \"4445\","
-        " \"-erl_epmd_port\", \"4445\","
-        " \"-setcookie\", \"secret\"]\n",
-    ct:log(info, ?LOW_IMPORTANCE, "Dockerfile:\n~s", [Dockerfile]),
-    ok = file:write_file(BuildScript, Dockerfile),
-    DockerBuildResult = os:cmd("docker build -t " ++ ReleaseName ++ " ."),
-    ct:log(info, ?LOW_IMPORTANCE, "Docker build:\n~s", [DockerBuildResult]).
